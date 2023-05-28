f3e37b1c518f00ef69a315a6901d5d9204b5a763
Adesanya07 <gadesanya22@gmail.com>
2023-05-28 18:47:37 +0100

    days

---
+++ DAY1/DAY2/weeks.py
@@ -0,0 +1,13 @@
+age = input("What is your current age? ")
+# 🚨 Don't change the code above 
+age_int = int(age)
+years_remaining = 90 - age_int
+weeks_remaining = years_remaining * 52
+days_remaining = years_remaining * 365
+months_remaining = years_remaining * 12
+print(f"you have {days_remaining} days, {weeks_remaining} weeks, {months_remaining} months left")
+
+
+
+
+
